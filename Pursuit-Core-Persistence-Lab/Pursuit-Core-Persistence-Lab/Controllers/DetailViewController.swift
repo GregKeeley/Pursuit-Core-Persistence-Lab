f789d48bee8ef6440653b6b4d7faca70c822da11
Gregory Keeley <gregorykeeley@pursuit.org>
2020-01-21 16:56:12 -0500

    - Created files for each view controller

---
+++ Pursuit-Core-Persistence-Lab/Pursuit-Core-Persistence-Lab/Controllers/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Pursuit-Core-Persistence-Lab
+//
+//  Created by Gregory Keeley on 1/21/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
